c2709f731d465cecd5c1a0f12f87c0598310ba1e
Jack Gezalian <jgezalianpro@gmail.com>
2026-06-03 23:07:45 -0700

    restructure

--- classifier/model/rf.py
+++ classifier/model/rf.py
@@ -8,14 +8,16 @@ from sklearn.metrics import precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 
-features_csv = "../features/master_features_labeled.csv"
-bumps_test_csv = "../features/bumps_test_ready.csv"
-X_bumps_test = pd.read_csv(bumps_test_csv)
-X_bumps_test = X_bumps_test.drop(columns=["label"])
+train_features_csv = "../features/train/master_train_features.csv"
+test_features_csv = "../features/test/master_test_features.csv"
 
-X = pd.read_csv(features_csv)
-X = X.drop(columns=["label"])
-y = pd.read_csv(features_csv)["label"]
+train = pd.read_csv(train_features_csv)
+test = pd.read_csv(test_features_csv)
+test_classes = test["label"]
+test_features = test.drop(columns="label")
+
+X = train.drop(columns=["label"])
+y = pd.read_csv(train_features_csv)["label"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X,
@@ -26,23 +28,18 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 rnd_clf = RandomForestClassifier(
-    n_estimators=500, max_depth=5, n_jobs=-1, random_state=42, class_weight="balanced"
+    n_estimators=500, max_depth=5, n_jobs=-1, random_state=42, #class_weight="balanced"
 )
 
 rnd_clf.fit(X_train, (y_train))
-# y_pred_rf = rnd_clf.predict(X_test)
 
-# rf_score = rnd_clf.score(X_test, y_test)
-# print(rf_score)
+predicted_classes = rnd_clf.predict(test_features)
 
-predicted_classes = rnd_clf.predict(X_bumps_test)
-print(predicted_classes)
+results = pd.DataFrame({
+    "actual": test_classes,
+    "predicted": predicted_classes,
+})
+results.to_csv("predicted_classes.csv", index=False)
 
-# df = pd.DataFrame(
-#     {
-#         "y_test": y_test["label"].reset_index(drop=True),
-#         "y_pred": pd.Series(y_pred_rf),
-#     }
-# )
 
-# df.to_csv("compare.csv")
+
